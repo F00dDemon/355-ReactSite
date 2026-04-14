b16f6711315952d365d9f3afbd1cddff0ed53ce1
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-04-14 11:06:57 -0700

    nuke css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Page from "./components/Page";
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <Page />
+      <Page />
+      <Page />
     </div>
   );
 }
