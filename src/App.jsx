be54c07b2be02aa3b5e123700c2ca89f2b27b52d
f00d <delossantos.orion@student.greenriver.edu>
2026-04-14 10:54:10 -0700

    uhh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
+import Header from "./components/Header.Jsx";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
 
 export default App;
