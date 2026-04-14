784c41af46607a31a7dc500064b1c077cb0efac1
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-04-14 10:54:27 -0700

    dsdsd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 function Header() {
-  return;
-  <div>
-    <header className="headerBar">
-      <h1>Italian Food</h1>
-      <nav className="navBar">
-        <a href="#">Food 1</a>
-        <a href="#">Food 2</a>
-        <a href="#">Food 3</a>
-      </nav>
-    </header>
-  </div>;
+  return (
+    <div>
+      <header className="headerBar">
+        <h1>Italian Food</h1>
+        <nav className="navBar">
+          <a href="#">Food 1</a>
+          <a href="#">Food 2</a>
+          <a href="#">Food 3</a>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
