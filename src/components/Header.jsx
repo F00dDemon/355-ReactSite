3c73131bdcd1aa2207a169e883447c6c00ee282b
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-04-15 21:12:01 -0700

    changed navbar names

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ export default function Header() {
     <header className="headerBar">
       <h1>Italian Food</h1>
       <nav className="navBar">
-        <a href="#">Food 1</a>
-        <a href="#">Food 2</a>
-        <a href="#">Food 3</a>
+        <a href="#">Risotto</a>
+        <a href="#">Mushroom Veal</a>
+        <a href="#">Pizza</a>
       </nav>
     </header>
   );
