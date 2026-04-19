6ad1191d65658257eb066c17b5c796221c01c30b
f00d <delossantos.orion@student.greenriver.edu>
2026-04-18 20:39:00 -0700

    pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,8 @@ import "./Hero.css";
 export default function Hero() {
   return (
     <div className="hero">
-      <img src="public\imgs\ifood.jpg" alt="" />
+      <img src="src/assets/ifood.jpg" />
+
     </div>
   );
 }
