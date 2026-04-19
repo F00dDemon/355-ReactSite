83df6102300a618fd0933ba19837c9380f95ee42
f00d <delossantos.orion@student.greenriver.edu>
2026-04-18 20:55:37 -0700

    imgs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import "./Hero.css";
 export default function Hero() {
   return (
     <div className="hero">
-      <img src="src/assets/ifood.jpg" />
+      <img src="/ifood.jpg" alt="italian food" />
 
     </div>
   );
